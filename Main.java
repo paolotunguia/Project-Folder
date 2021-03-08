fbbe2730f644bad88671b36ee9a637a92dde2a8b
paolotunguia <79895472+paolotunguia@users.noreply.github.com>
2021-03-08 12:09:10 +0800

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class assignment004
+{
+	public static void main(String[] args) {
+		System.out.println("Paolo Louise Tunguia\n");
+		System.out.println("Godspeed Guinto\n");
+		System.out.println("Joaquinn Quilat\n");
+		System.out.println("Renz Beaver Layco\n");
+		System.out.println("Jasper Kyle Santos\n");
+	}
+}
