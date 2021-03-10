9f3e218625d291cf7383655fb07cbb7e3f82e412
paolotunguia <79895472+paolotunguia@users.noreply.github.com>
2021-03-10 13:46:12 +0800

    first commit

--- Main.java
+++ Main.java
@@ -1,10 +1,32 @@
-public class assignment004
-{
-	public static void main(String[] args) {
-		System.out.println("Paolo Louise Tunguia\n");
-		System.out.println("Godspeed Guinto\n");
-		System.out.println("Joaquinn Quilat\n");
-		System.out.println("Renz Beaver Layco\n");
-		System.out.println("Jasper Kyle Santos\n");
-	}
+public class Main {
+
+    public static void main(String[] args) {
+        Person first = new Person();
+        Person second = new Person();
+        Person third = new Person();
+        Person fourth = new Person();
+        Person fifth = new Person();
+
+        first.setFirstName("Paolo");
+        first.setLastName("Tunguia");
+
+        second.setFirstName("Godspeed");
+        second.setLastName("Guinto");
+
+        third.setFirstName("Renz");
+        third.setLastName("Layco");
+
+        fourth.setFirstName("Joaquinn");
+        fourth.setLastName("Quilat");
+
+        fifth.setFirstName("Jasper");
+        fifth.setLastName("Santos");
+
+        System.out.println("Person 1: "+ first.getFirstname() + first.getLastName());
+        System.out.println("Person 2: "+ second.getFirstname()+ second.getLastName());
+        System.out.println("Person 3: "+ third.getFirstname()+ third.getLastName());
+        System.out.println("Person 4: "+ fourth.getFirstname()+ fourth.getLastName());
+        System.out.println("Person 5: "+ fifth.getFirstname()+ fifth.getLastName());
+
+    }
 }
--- Person.java
+++ Person.java
@@ -1,6 +1,17 @@
-public class assignment004
-{
-	public static void main(String[] args) {
-		System.out.println("Paolo Louise Tunguia");
+public class Person {
+	private String firstName;
+	private String lastName;
+
+	public String getFirstname() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 }
